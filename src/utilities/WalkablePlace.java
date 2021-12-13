14f83f54604e0de05ecfce4fe0fb6526f9110928
Sergey <68108852+k1b24@users.noreply.github.com>
2021-12-13 22:18:38 +0300

    Update WalkablePlace.java

--- src/utilities/WalkablePlace.java
+++ src/utilities/WalkablePlace.java
@@ -1,16 +1,75 @@
 package utilities;
 
+import exceptions.InvalidNameException;
+import exceptions.NullObjectException;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
-public interface WalkablePlace extends ObjectInterface {
-    boolean hasWalkers = false;
-    ArrayList<Person> currentWalkers = new ArrayList<>();
+public abstract class WalkablePlace implements ObjectInterface {
+    private final String name;
+    public boolean hasWalkers = false;
+    public StreetSideType streetSide;
+    public ArrayList<Person> currentWalkers = new ArrayList<>();
 
-    StreetSideType getStreetSide();
+    public WalkablePlace(String name) throws InvalidNameException {
+        if (name.isEmpty() || name == null) {
+            throw new InvalidNameException("Название объекта ландшафта некорректно");
+        } else {
+            this.name = name;
+        }
+    }
 
-    void deleteWalker(Person walker);
+    public static enum StreetSideType {
+        LEFT_SIDE,
+        RIGHT_SIDE
+    }
 
-    void addWalker(Person walker);
+    public void addWalker(Person walker) throws NullObjectException {
+        if (walker == null) {
+            throw new NullObjectException("В метод addWalker передан пустой объект");
+        } else {
+            this.currentWalkers.add(walker);
+            this.hasWalkers = true;
+        }
+    }
 
-    void setStreetSide(StreetSideType streetSide);
+    public void deleteWalker(Person walker) throws NullObjectException {
+        if (walker == null) {
+            throw new NullObjectException("В метод deleteWalker передан пустой объект");
+        } else {
+            this.currentWalkers.remove(walker);
+            if (this.currentWalkers.isEmpty()) {
+                this.hasWalkers = false;
+            }
+        }
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Walkable place's name: '" + getName() + "', current walkers: " + currentWalkers;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        WalkablePlace place = (WalkablePlace) obj;
+
+        return name.equals(place.name) &&
+                hasWalkers == place.hasWalkers &&
+                streetSide.equals(place.streetSide) &&
+                currentWalkers.equals(place.currentWalkers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, hasWalkers, currentWalkers, streetSide);
+    }
 }
